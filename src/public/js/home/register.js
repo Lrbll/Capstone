5883a97d1fc7b72d151887b425ef9d1722d55dab
Lrbll <dlxkdxs@naver.com>
2023-04-09 16:45:13 +0900

    회원가입 라우팅

--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -9,11 +9,14 @@ console.log("hello register");
 register_btn.addEventListener("click", register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (password.value !== confirm_password)
+    return alert("비밀번호가 일치하지 않습니다.");
+
   const req = {
     id: id.value,
     name: name.value,
     password: password.value,
-    confirm_password: confirm_password.value,
   };
   console.log(req);
 
