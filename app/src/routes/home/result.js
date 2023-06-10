34581653b3bb2ae05192460786a261c4904e0d3a
Lrbll <dlxkdxs@naver.com>
2023-06-10 22:33:02 +0900

    유저별 진단결과 출력

--- app/src/routes/home/result.js
+++ app/src/routes/home/result.js
@@ -1,7 +1,92 @@
 "use strict";
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 let db = require("../../config/db");
+
+// url에 대한 페이지 처리 로직을 담당하는 핸들러 함수
+const urlHandler = (req, res, url) => {
+  const is_logined = req.session.is_logined;
+  console.log(url);
+
+  // url에 해당하는 페이지 처리 로직
+  const query = `SELECT results FROM results_info WHERE id = '${req.session.nickname}' AND url = '${url}' ORDER BY num DESC LIMIT 1`;
+
+  db.mysql.query(query, (err, results) => {
+    if (err) {
+      console.error("Error querying the database:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    if (results.length === 0) {
+      // 해당하는 결과가 없는 경우 처리
+      res.status(404).send("Results not found");
+      return;
+    }
+
+    const jsonData = JSON.parse(results[0].results);
+    const {
+      AE,
+      BA,
+      BF,
+      BS,
+      DL,
+      SF,
+      SM,
+      DOR,
+      RFA,
+      XSS,
+      Base,
+      CSRF,
+      LDAP,
+      Cookie,
+      PHP_CI,
+      Redirect,
+      SI_Login,
+      SI_Search,
+      XML_XPATH,
+      XSS_Stored,
+    } = jsonData;
+    console.log(jsonData);
+
+    // 파일에서 점검항목 info 가져오기
+    fs.readFile("src/scripts_info/scripts.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error reading JSON file");
+        return;
+      }
+
+      const scripts = JSON.parse(data);
+
+      res.render("home/result2", {
+        is_logined: is_logined,
+        scripts: scripts,
+        AE,
+        BA,
+        BF,
+        BS,
+        DL,
+        SF,
+        SM,
+        DOR,
+        RFA,
+        XSS,
+        Base,
+        CSRF,
+        LDAP,
+        Cookie,
+        PHP_CI,
+        Redirect,
+        SI_Login,
+        SI_Search,
+        XML_XPATH,
+        XSS_Stored,
+      });
+    });
+  });
+};
 
 router.get("/", (req, res) => {
   const is_logined = req.session.is_logined;
@@ -25,81 +110,13 @@ router.get("/", (req, res) => {
     // 고유한 url 값들을 기반으로 페이지를 렌더링합니다.
     urls.forEach((url) => {
       router.get(`/${url}`, (req, res) => {
-        // url에 해당하는 페이지 처리 로직
-        res.send(`Welcome to ${url}'s page!`);
+        urlHandler(req, res, url);
       });
     });
 
-    // // DB에서 진단도구 결과 가져오기
-    // const query = "SELECT results FROM json_db";
-    // db.mysql.query(query, (err, results) => {
-    //   if (err) {
-    //     console.error("Error querying the database:", err);
-    //     res.status(500).send("Internal Server Error");
-    //     return;
-    //   }
-
-    //   const jsonData = JSON.parse(results[0].results);
-    //   const {
-    //     AE,
-    //     BA,
-    //     BF,
-    //     BS,
-    //     DL,
-    //     SF,
-    //     SM,
-    //     DOR,
-    //     RFA,
-    //     XSS,
-    //     Base,
-    //     CSRF,
-    //     LDAP,
-    //     Cookie,
-    //     PHP_CI,
-    //     Redirect,
-    //     SI_Login,
-    //     SI_Search,
-    //     XML_XPATH,
-    //     XSS_Stored,
-    //   } = jsonData;
-    //   console.log(jsonData);
-
-    //   // 파일에서 점검항목 info 가져오기
-    //   fs.readFile("src/scripts_info/scripts.json", "utf8", (err, data) => {
-    //     if (err) {
-    //       console.error(err);
-    //       res.status(500).send("Error reading JSON file");
-    //       return;
-    //     }
-
-    //     const scripts = JSON.parse(data);
     res.render("home/result", {
       is_logined: is_logined,
       urls: urls,
-
-      // scripts: scripts,
-      // AE,
-      // BA,
-      // BF,
-      // BS,
-      // DL,
-      // SF,
-      // SM,
-      // DOR,
-      // RFA,
-      // XSS,
-      // Base,
-      // CSRF,
-      // LDAP,
-      // Cookie,
-      // PHP_CI,
-      // Redirect,
-      // SI_Login,
-      // SI_Search,
-      // XML_XPATH,
-      // XSS_Stored,
-      // });
-      //   });
     });
   });
 });
